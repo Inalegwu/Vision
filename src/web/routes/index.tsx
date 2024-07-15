41537781deef49dcc74029f2b0f0f53604c5e16e
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2024-07-15 18:15:43 +0000

    [chore]cleanup

--- src/web/routes/index.tsx
+++ src/web/routes/index.tsx
@@ -6,33 +6,6 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  const mock = [
-    {
-      id: 1,
-    },
-    {
-      id: 2,
-    },
-    {
-      id: 3,
-    },
-    {
-      id: 4,
-    },
-    {
-      id: 5,
-    },
-    {
-      id: 6,
-    },
-    {
-      id: 7,
-    },
-    {
-      id: 8,
-    },
-  ];
-
   return (
     <Flex direction="column" className="h-screen w-full">
       <Flex
