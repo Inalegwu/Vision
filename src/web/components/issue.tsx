69678be6f19a6768d6c65fe57090bbcdf32664eb
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2024-07-30 03:27:51 +0000

    still trying to figure out touch events on issue component

--- src/web/components/issue.tsx
+++ src/web/components/issue.tsx
@@ -1,9 +1,10 @@
 import { deleteFromStoreCompletionEvent$ } from "@core/events";
-import { ContextMenu, Flex, Text } from "@radix-ui/themes";
+import { DropdownMenu, Flex, Text } from "@radix-ui/themes";
 import t from "@shared/config";
 import type { Issue as issue } from "@src/shared/types";
 import { useRouter } from "@tanstack/react-router";
-import { Edit, Trash2 } from "lucide-react";
+import { Edit2, Link2Off, MoreVertical } from "lucide-react";
+import { memo } from "react";
 
 type Props = {
   issue: issue;
@@ -12,56 +13,83 @@ type Props = {
 export default function Issue({ issue }: Props) {
   const utils = t.useUtils();
   const navigation = useRouter();
-  const { mutate: deleteIssue } = t.issue.deleteIssue.useMutation();
 
   deleteFromStoreCompletionEvent$.on(() => utils.library.invalidate());
 
   return (
-    <ContextMenu.Root>
-      <ContextMenu.Trigger>
-        <Flex
-          className="w-[200px] h-[300px] cursor-pointer mb-14"
-          gap="1"
-          direction="column"
-          onClick={() =>
-            navigation.navigate({
-              to: "/$issueId",
-              params: {
-                issueId: issue.id,
-              },
-            })
-          }
-        >
-          <img
-            className="w-full h-full bg-zinc-200/5 rounded-md border-1 border-solid border-zinc-300 dark:border-zinc-800"
-            alt="issue_thumbnail"
-            src={issue.thumbnailUrl}
-          />
-          <Flex direction="column" align="start">
-            <Text size="1" className="text-gray-600 dark:text-zinc-400">
-              {issue.issueTitle}
-            </Text>
+    <Flex
+      className="w-[200px] h-[300px] mb-14 cursor-pointer"
+      gap="1"
+      direction="column"
+      onClick={() =>
+        navigation.navigate({
+          to: "/$issueId",
+          params: {
+            issueId: issue.id,
+          },
+        })
+      }
+    >
+      <img
+        className="w-full h-full bg-zinc-200/5 rounded-md border-1 border-solid border-zinc-300 dark:border-zinc-800"
+        alt="issue_thumbnail"
+        src={issue.thumbnailUrl}
+      />
+      <Flex align="end" justify="between">
+        <Text size="1" className="text-gray-600 dark:text-zinc-400">
+          {issue.issueTitle}
+        </Text>
+        <MoreButton issueId={issue.id} />
+      </Flex>
+    </Flex>
+  );
+}
+
+const MoreButton = memo(({ issueId }: { issueId: string }) => {
+  const utils = t.useUtils();
+  const navigation = useRouter();
+  const { mutate: deleteIssue } = t.issue.deleteIssue.useMutation({
+    onSuccess: () => utils.library.invalidate(),
+  });
+
+  const unlink = () =>
+    deleteIssue({
+      issueId,
+    });
+
+  const go = () =>
+    navigation.navigate({
+      to: "/edit/$id",
+      params: {
+        id: issueId,
+      },
+    });
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <button className="cursor-pointer">
+          <MoreVertical size={10} />
+        </button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content variant="soft" size="1">
+        <DropdownMenu.Item onClick={go} className="cursor-pointer">
+          <Flex align="center" justify="start" gap="1">
+            <Edit2 size={9} />
+            <Text>Edit Issue Info</Text>
           </Flex>
-        </Flex>
-      </ContextMenu.Trigger>
-      <ContextMenu.Content size="1" variant="soft">
-        <ContextMenu.Item className="cursor-pointer">
-          <Flex align="center" justify="start" gap="2">
-            <Edit size={10} />
-            <Text>Edit Issue</Text>
-          </Flex>
-        </ContextMenu.Item>
-        <ContextMenu.Item
-          onClick={() => deleteIssue({ issueId: issue.id })}
+        </DropdownMenu.Item>
+        <DropdownMenu.Item
+          onClick={unlink}
           color="ruby"
           className="cursor-pointer"
         >
-          <Flex align="center" justify="start" gap="2">
-            <Trash2 size={10} />
-            <Text>Delete Issue</Text>
+          <Flex align="center" justify="start" gap="1">
+            <Link2Off size={10} />
+            <Text>Unlink Issue</Text>
           </Flex>
-        </ContextMenu.Item>
-      </ContextMenu.Content>
-    </ContextMenu.Root>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
-}
+});
---
+++ src/web/routes/edit/$id.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@radix-ui/themes";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/edit/$id")({
+  component: Component,
+});
+
+function Component() {
+  const { id } = Route.useParams();
+
+  return <Flex className="w-full h-screen">{id}</Flex>;
+}
